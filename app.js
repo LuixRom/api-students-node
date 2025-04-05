a82cea4c8a2a8aed5c5c31509895b5927c6707f6
LuixRom <luis.romero@utec.edu.pe>
2025-04-05 13:07:19 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.delete("/student/:id", (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
